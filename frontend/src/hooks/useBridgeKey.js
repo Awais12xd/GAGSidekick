1d04fb6c470b746a4863efe2d17cd99e019389a5
Awais12xd <lucifer.awaisxx213@example.com>
2025-08-11 21:07:01 +0500

    Add comment to clarify payload normalization in useBridgeKey

--- frontend/src/hooks/useBridgeKey.js
+++ frontend/src/hooks/useBridgeKey.js
@@ -48,6 +48,7 @@ export default function useBridgeKey({
     })();
 
     /**
+     * skjdh
      * Normalize incoming payloads to a 'body' that likely contains the resource.
      * Common shapes:
      *  - { weather: [...] }
