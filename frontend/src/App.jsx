b11ed56694059961e1f24800484d28e56dcd59bd
Awais12xd <lucifer.awaisxx213@example.com>
2025-08-12 06:51:50 +0500

    Add @vercel/analytics dependency and integrate Analytics component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home.jsx";
+import { Analytics } from '@vercel/analytics/react';
+
  // move your current app content to this
 
 const App = () => {
   return (
+   <>
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    <Analytics />
+   </>
   );
 };
 
